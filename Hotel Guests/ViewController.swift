76674e1e3c5370844f4c6fc68bc329d27c1222c5
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-02-18 18:04:02 +0000

    Initial Commit

---
+++ Hotel Guests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel Guests
+//
+//  Created by Jonathan Burnett on 18/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
