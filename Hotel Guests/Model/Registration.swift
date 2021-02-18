d2ba932985004ad5cf58132c9d02e0b3e61d5c81
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-02-18 21:15:19 +0000

    Upload Completed Project

--- Hotel Guests/Model/Registration.swift
+++ Hotel Guests/Model/Registration.swift
@@ -7,16 +7,21 @@
 
 import Foundation
 
+// Registration Model
 struct Registration {
+    
+    static let wifiCost = 10
+    
     var firstName: String
     var lastName: String
     var emailAddress: String
     
     var checkInDate: Date
     var checkOutDate: Date
-    var numberOfAdluts: Int
+    var numberOfAdults: Int
     var numberOfChildren: Int
     
     var roomType: RoomType
     var wifi: Bool
+    
 }
--- Hotel Guests/Model/RoomType.swift
+++ Hotel Guests/Model/RoomType.swift
@@ -7,15 +7,25 @@
 
 import Foundation
 
-// Room Type Struct
+// Room Type Model
 struct RoomType: Equatable {
     var id: Int
     var name: String
     var shortName: String
     var price: Int
+    
+    // Array of Room Types of from the RoomType Property
+    static var all: [RoomType] {
+        return [RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+                RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+                RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)]
+    }
+    
 }
- 
-func == ( 1hs: RoomType, rhs: RoomType) -> Bool {
-    return 1hs.id == rhs.id
-} 
+
+// Equatable Protocol Implemtation fro RoomType
+func ==(lhs: RoomType, rhs: RoomType) -> Bool {
+    return lhs.id == rhs.id
+}
+
 
